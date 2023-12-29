3647943d1e0d0259645c223bbf7797a8c46e73ea
minhong1126 <mjhong159357@gmail.com>
2023-12-29 23:58:31 +0900

    #1-3: 라우터 추가

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-const express = require('express');
+const express = require("express");
 const path = require("path");
+const route = require("./routes/routes");
 const app = express();
-const router = express.Router();
 const port = 8000;
 
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
-app.get('/', (req, res) => { 
-  res.render("index");
-});
-app.get('./route/signUp', (req, res) => {
-
-})
+app.use("/", route);
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
