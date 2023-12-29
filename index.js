993bd33aa02ecd64a2ee75d47389fb9a821d4641
minhong1126 <mjhong159357@gmail.com>
2023-12-30 00:25:20 +0900

    #1-3-1: PR오류 변경 커밋

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const route = require("./routes/routes");
 const app = express();
 const port = 8000;
 
+// 깃 PR오류때문에 변경사항 만들기 우우우우우
+
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
