f2bd0123b762fb256c53e482656c660a34cc3b7a
minhong1126 <mjhong159357@gmail.com>
2024-01-06 15:26:43 +0900

    #1-4: 중간 커밋

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require("express");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
 const path = require("path");
 const route = require("./routes/routes");
+
 const app = express();
 const port = 8000;
 
-// 깃 PR오류때문에 변경사항 만들기 우우우우우
-
+// pug 설정
 app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 app.use("/", route);
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser('secret code'));
+
+// 세션 설정
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    httpOnly: true,
+    secure: false
+  }
+}));
+
+// 에러 처리
+app.use(function(err, req, res, next){
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") == "development" ? err : {};
+
+  res.status(err.status || 500);
+  res.render("error");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -2,9 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => { 
-    res.render("home/index");
-  });
-router.get('signUp', (req, res) => {
+    res.render("index");
+});
+router.get('/signup', (req, res) => {
     res.render("signup/signUp");
 });
 
